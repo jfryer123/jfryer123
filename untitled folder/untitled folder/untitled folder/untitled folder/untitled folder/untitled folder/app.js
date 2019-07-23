101d954a80c84f39e0e6dd4d34ca0b6d80088931
jfryer123 <51830458+jfryer123@users.noreply.github.com>
2019-07-23 12:32:09 -0400

    Add files via upload

---
+++ untitled folder/untitled folder/untitled folder/untitled folder/untitled folder/untitled folder/app.js
@@ -0,0 +1,170 @@
+$(() => {
+
+
+// const finished = [];
+//
+
+
+
+
+        // $(document).ready(function(){
+        // $('#submit').trigger(':reset');
+        // $(function(){
+        // $('#shares').val('');
+        // });
+        // });
+        // $('#form-id')[0].reset();
+
+
+        // readingList.push(userInput)
+        //
+        // render();
+    // const render = () => {
+        //   $('.readingList').empty();
+        //  readingList.forEach((item) => {
+        //    $('ul').append('<li>' + item + '</li>');
+        //  });
+        // }
+        // console.log('list: ', list);
+        //   list.forEach((item) => {
+        //     console.log(item);
+        //   });
+        // }
+
+const list = [];
+const $closeBtn = $('#close');
+
+    const render = () => {
+        // $('.readingList').empty();
+        // list.forEach((item) => {
+        // for (let i =0; i < list.length; i++) {
+        var $items = $("<div class='listitems'>")
+        // var $lastitem = $(array[array.length-1])
+        $($items).append("<li>" + list[list.length-1] + "</li>")
+        $('.readingList').append($items)
+        $($items).append($("<button>").text("Finished!").addClass("finished"))
+
+        // const rate = () => {
+        // var promptAnswer = prompt("Did you like the book?", "yes or no");
+        //   if(promptAnswer == "yes") {
+        //     $($items).append($(".like").show())
+        //   } else {
+        //     $(".dislike").show()
+        //   }
+        // $($items).append($("<button>").text("Nevermind").addClass("nevermind"))
+
+
+
+    }
+
+
+        //   $('.readingList').append('<li>' + item + '</li>');
+        //   $("li").append($("<button>").text("Finished!").addClass("finished"))
+        // });
+        // }
+    // const done = () => {
+    //     // $("button").empty();
+    //     list.forEach((item)=> {
+
+
+
+
+      //   })
+      // }
+      const closeModal = () => {
+        $("#modal").css('display', 'none');
+        // console.log('ijfdbv');
+      }
+
+
+
+    // const closeModal = () => {
+    //   $("#modal".css('display', 'none');
+    //   }
+
+
+    // const switched = (event) => {
+      // const $move = $(event.target).parent()
+      // $(".newList").append($move)
+      // $($move).addClass("done-item")
+      // const $removeBtn = $($move).children("button")
+      // $removeBtn.html("Remove")
+    //   console.log("iaefjbva");
+    // }
+
+
+///////////////////////this works /////////////////////////
+    $('form').on('submit', (event) => {
+      const userInput = $('input[type="text"]').val();
+      event.preventDefault();
+      $(event.currentTarget).trigger('reset');
+      list.push(userInput)
+
+    $.ajax({
+        url:'https://www.googleapis.com/books/v1/volumes?q=' + userInput,
+        type: "GET",
+        }).then(
+            (data) => {
+                // $("#cover").html(data.items[0].volumeInfo.imageLinks.thumbnail)
+                $("#title").html(data.items[0].volumeInfo.title);
+                $("#author").html(data.items[0].volumeInfo.authors)
+                $("#description").html(data.items[0].volumeInfo.description)
+            },
+            (error) => {
+                console.log(error);
+            }
+        );
+    });
+
+    $('.listed').on('click', (event) => {
+          render();
+          // done()
+          // $(event.target).remove()
+        })
+
+    $('.readingList').on('click', (event)=>{
+      $(event.target).parent().appendTo(".newList");
+      $(event.target).remove();
+      // rate();
+      var promptAnswer = prompt("Did you like the book?", "yes or no");
+        if(promptAnswer == "yes") {
+          $(event.target).parent($(".like").show())
+        } else {
+          $(".dislike").show()
+        }
+      });
+        // } if else {
+        //   $(event.target).parent().append("<button>").text("Don't recommend to friends");
+        // }
+
+
+      // rate();
+
+      // css('text-decoration', 'line-through');
+      // $(event.target).effect('shake');
+      // $(event.target).appendTo($(".newList"))
+      // switched()
+      // const $move = $(".finished").parent()
+      // $(".newList").append($move)
+      // console.log("kajfbv");
+
+
+
+
+
+
+    $closeBtn.on('click', (event) => {
+      closeModal();
+      $(".like").hide()
+      $(".dislike").hide()
+
+
+    })
+
+
+
+
+
+
+
+  });
